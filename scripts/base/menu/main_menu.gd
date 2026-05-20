6e4b1bcc0978789e764522751d6355fb84acd50b
GmbTheGreat <maroof9404@gmail.com>
2026-05-20 22:21:50 +0530

    UI update
    customization button added
    customization script button signal attached.

--- scripts/base/menu/main_menu.gd
+++ scripts/base/menu/main_menu.gd
@@ -14,5 +14,8 @@ func _process(delta: float) -> void:
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/menu/levels_menu.tscn")
 
+func _on_customize_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/menu/customization_menu.tscn")
+
 func _on_exit_pressed() -> void:
 	get_tree().quit()
