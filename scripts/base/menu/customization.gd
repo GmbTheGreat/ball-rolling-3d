999bbfb216d2971b878678833639d207a993dcf8
GmbTheGreat <maroof9404@gmail.com>
2026-05-25 14:02:24 +0530

    cosmetic category button press signal created

--- scripts/base/menu/customization.gd
+++ scripts/base/menu/customization.gd
@@ -44,3 +44,15 @@ func _on_equip_pressed() -> void:
 
 func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/menu/main_menu.tscn")
+
+
+func _on_ball_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_trail_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_background_pressed() -> void:
+	pass # Replace with function body.
