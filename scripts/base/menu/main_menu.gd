93294390b6c8fdc270e9e4fecfba23f99074ea5a
GmbTheGreat <maroof9404@gmail.com>
2026-05-26 22:34:09 +0530

    UI animation update

--- scripts/base/menu/main_menu.gd
+++ scripts/base/menu/main_menu.gd
@@ -2,20 +2,68 @@ extends Node3D
 
 @onready var background: WorldEnvironment = $WorldEnvironment
 
-# Called when the node enters the scene tree for the first time.
+@onready var start_btn = $Buttons/Start
+@onready var customize_btn = $Buttons/Customize
+@onready var settings_btn = $Buttons/Settings
+@onready var exit_btn = $Buttons/Exit
+
+
 func _ready() -> void:
 	apply_equipped_background()
 
 
-func apply_equipped_background():
+func apply_equipped_background() -> void:
 	var bg = CosmeticsManager.get_equipped_background()
 	background.environment.sky.sky_material.panorama = bg["hdri"]
+
+
+func hover_in(button: Control) -> void:
+	create_tween().tween_property(button, "scale", Vector2(1.08, 1.08), 0.12)
+
+
+func hover_out(button: Control) -> void:
+	create_tween().tween_property(button, "scale", Vector2.ONE, 0.12)
+
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/menu/levels_menu.tscn")
 
+
 func _on_customize_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/menu/customization.tscn")
 
+
 func _on_exit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_start_mouse_entered() -> void:
+	hover_in(start_btn)
+
+
+func _on_start_mouse_exited() -> void:
+	hover_out(start_btn)
+
+
+func _on_customize_mouse_entered() -> void:
+	hover_in(customize_btn)
+
+
+func _on_customize_mouse_exited() -> void:
+	hover_out(customize_btn)
+
+
+func _on_settings_mouse_entered() -> void:
+	hover_in(settings_btn)
+
+
+func _on_settings_mouse_exited() -> void:
+	hover_out(settings_btn)
+
+
+func _on_exit_mouse_entered() -> void:
+	hover_in(exit_btn)
+
+
+func _on_exit_mouse_exited() -> void:
+	hover_out(exit_btn)
