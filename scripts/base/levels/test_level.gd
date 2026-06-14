4b10981ea8211e9e9bc2cfd79ff57d6be3da4fd3
GmbTheGreat <maroof9404@gmail.com>
2026-06-14 23:14:11 +0530

    hammer added

--- scripts/base/levels/test_level.gd
+++ scripts/base/levels/test_level.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 @onready var visible_checkpoint: Node3D = $Checkpoints/visible_checkpoint
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _ready() -> void:
 	visible_checkpoint.deactivate()
